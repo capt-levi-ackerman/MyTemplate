1dce025ecdff3836b89ca925f542a30ae01f6d9a
SATYAM KUMAR <100135781+dknite@users.noreply.github.com>
2022-12-26 00:02:30 +0530

    New Mod Functions Updated
    ADD, SUB, MUL, POWERM, INVERSE

--- kickstart.cpp
+++ kickstart.cpp
@@ -30,6 +30,53 @@ using namespace std;
 #define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
 #define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
 
+#------------------------------------ MOD Operations (Unleash the Beast Mode) ----------------------------------
+# Just declare a variable mod with value and use these functions.
+
+
+inline void add(int &a, int b) {
+  a += b;
+  if (a >= mod) a -= mod;
+}
+
+inline void sub(int &a, int b) {
+  a -= b;
+  if (a < 0) a += mod;
+}
+
+inline int mul(int a, int b) {
+  return (int) ((long long) a * b % mod);
+}
+
+inline int powerM(int a, long long b) {
+  int res = 1;
+  while (b > 0) {
+    if (b & 1) {
+      res = mul(res, a);
+    }
+    a = mul(a, a);
+    b >>= 1;
+  }
+  return res;
+}
+
+inline int inv(int a) {
+  a %= mod;
+  if (a < 0) a += mod;
+  int b = mod, u = 0, v = 1;
+  while (a) {
+    int t = b / a;
+    b -= t * a; swap(a, b);
+    u -= t * v; swap(u, v);
+  }
+  assert(b == 1);
+  if (u < 0) u += mod;
+  return u;
+}
+
+#--------------------------------------------------------------------------
+
+
 inline int power(int a, int b)
 {
     int x = 1;
